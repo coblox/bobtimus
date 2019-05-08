11bbed09f728ccee6e202331c99e8483a3c8b988
Philipp Hoenisch <philipp@hoenisch.at>
2019-05-08 13:16:50 +1000

    Move back to rxjs
    Signed-off-by: Philipp Hoenisch <philipp@hoenisch.at>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { ActionHandler } from "./action_handler";
-import { ComitNode, Swap } from "./comit_node_api";
-import { selectAction } from "./decision";
+import {from, timer} from "rxjs";
+import {ActionHandler} from "./action_handler";
+import {ComitNode, Swap} from "./comit_node_api";
+import {selectAction} from "./decision";
+import {Entity} from "../gen/siren";
+import {map, mergeMap} from 'rxjs/operators';
+import {Result} from "@badrap/result/dist";
+import {Response} from "request";
 
 // Modules
 // comit-node-api: handle queries to comit node and configuration of end point?
@@ -9,23 +14,27 @@ import { selectAction } from "./decision";
 const comitNode = new ComitNode();
 const actionHandler = new ActionHandler();
 
-async function processSwaps() {
-  const swaps = await comitNode.getSwaps();
+async function process(entity: Entity): Promise<Result<Response, Error>> {
+    if (entity.class && entity.class.some((e: string) => e === "swap")) {
+        const swap = entity as Swap;
 
-  swaps.forEach(async (entity: any) => {
-    if (entity.class.some((e: string) => e === "swap")) {
-      const swap = entity as Swap;
+        const nextAction = selectAction(swap);
 
-      const nextAction = selectAction(swap);
-
-      if (nextAction.isOk) {
-        const action = nextAction.unwrap();
-        console.log("Will do " + action.title);
-        let res = await actionHandler.triggerAction(action);
-        console.log(res);
-      }
+        if (nextAction.isOk) {
+            const action = nextAction.unwrap();
+            console.log("Will do " + action.title);
+            return actionHandler.triggerAction(action);
+        }
     }
-  });
+    return Result.err(new Error("Internal Error"));
 }
 
-processSwaps();
+timer(0, 500)
+    .pipe(() => from(comitNode.getSwaps()))
+    .pipe(mergeMap(swap => from(swap).pipe(map(swap => process(swap)))))
+    .pipe(action => from(action))
+    .subscribe(
+        (swap) => console.log("success: " + swap),
+        (error) => console.error("error: " + error),
+        () => console.log("dosne")
+    );

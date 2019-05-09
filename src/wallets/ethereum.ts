3353f4d33318e5caa1d845bfa3c7f18cf15362c1
Philipp Hoenisch <philipp@hoenisch.at>
2019-05-09 17:20:53 +1000

    Add EthereumWallet
    Signed-off-by: Philipp Hoenisch <philipp@hoenisch.at>

---
+++ src/wallets/ethereum.ts
@@ -0,0 +1,78 @@
+import BN from "bn.js";
+import EthereumTx from "ethereumjs-tx";
+import utils from "ethereumjs-util";
+import Web3 from "web3";
+
+interface TransactionParams {
+  to?: string;
+  data?: Buffer;
+  value?: BN;
+  gasPrice: BN;
+  gasLimit: BN;
+}
+
+export class EthereumWallet {
+  private web3: Web3;
+  private chainId: number;
+  private privateKey: Buffer;
+  private account: string;
+
+  constructor(web3: Web3, privateKey: Buffer, chainId: number) {
+    this.chainId = chainId;
+    this.web3 = web3;
+    this.account = "0x" + utils.privateToAddress(privateKey).toString("hex");
+    this.privateKey = privateKey;
+  }
+
+  public async sendTransactionTo(params: TransactionParams) {
+    if (!params.to && !params.data) {
+      throw new Error("A transaction without `to` and `data` is not valid.");
+    }
+
+    const tx = await this.paramsToTransaction(params);
+
+    return this.signAndSend(tx);
+  }
+
+  public async deployContract(params: TransactionParams) {
+    if (params.to) {
+      throw new Error("Contract deployments must not set a `to` address.");
+    }
+
+    if (!params.data) {
+      throw new Error("Contract deployments must have `data`.");
+    }
+
+    const tx = await this.paramsToTransaction(params);
+
+    return this.signAndSend(tx);
+  }
+
+  private async paramsToTransaction({
+    to,
+    data,
+    value = new BN(0),
+    gasLimit,
+    gasPrice
+  }: TransactionParams) {
+    const nonce = await this.web3.eth.getTransactionCount(this.account);
+
+    return new EthereumTx({
+      nonce: `0x${new BN(nonce).toString("hex")}`,
+      gasPrice: `0x${gasPrice.toString("hex")}`,
+      gasLimit: `0x${gasLimit.toString("hex")}`,
+      to: to ? to : null,
+      data: data ? data : null,
+      value: `0x${value.toString("hex")}`,
+      chainId: this.chainId
+    });
+  }
+
+  private async signAndSend(tx: EthereumTx) {
+    tx.sign(this.privateKey);
+    const serializedTx = tx.serialize();
+    const hex = "0x" + serializedTx.toString("hex");
+
+    return this.web3.eth.sendSignedTransaction(hex);
+  }
+}
---
+++ tests/parityTestContainer.ts
@@ -0,0 +1,15 @@
+import { GenericContainer } from "testcontainers";
+
+export default function parityTestContainer() {
+  return new GenericContainer("parity/parity", "v2.5.0")
+    .withCmd([
+      "--config=dev",
+      "--jsonrpc-apis=all",
+      "--unsafe-expose",
+      "--tracing=on",
+      "--logging=debug,ethcore-miner=trace,miner=trace,rpc=trace,tokio_core=warn,tokio_reactor=warn",
+      "--jsonrpc-cors='all'"
+    ])
+    .withExposedPorts(8545)
+    .start();
+}

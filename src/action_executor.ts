2b7fc71a311b087016f348f39b557a9b3e842077
Philipp Hoenisch <philipp@hoenisch.at>
2019-05-09 11:04:40 +1000

    Use URIjs
    Signed-off-by: Philipp Hoenisch <philipp@hoenisch.at>

--- src/action_executor.ts
+++ src/action_executor.ts
@@ -1,9 +1,9 @@
-import {Response} from "request";
+import { Response } from "request";
 import request from "request-promise-native";
-import {Action, Field} from "../gen/siren";
+import { Action, Field } from "../gen/siren";
 import config from "./config";
-import {Datastore} from "./datastore";
-
+import { Datastore } from "./datastore";
+import URI from "urijs";
 
 export class ActionExecutor {
   public datastore: Datastore;
@@ -12,22 +12,20 @@ export class ActionExecutor {
     this.datastore = datastore;
   }
 
-  public async execute(
-    action: Action,
-  ): Promise<Response> {
+  public async execute(action: Action): Promise<Response> {
     let body: any = {};
-    let url = config.prependUrlIfNeeded(action.href);
+    let prepended_url: string = config.prependUrlIfNeeded(action.href);
+    var url = new URI(prepended_url);
 
     if (action.fields) {
-      const promises = action.fields.map(async (field) => {
+      const promises = action.fields.map(async field => {
         const data: any = await this.retrieveDataForField(field);
         if (data) {
           if (action.method === "GET") {
             console.log(data);
-            url += "?";
-            url += Object.entries(data)
+            Object.entries(data)
               .map(([key, val]) => `${key}=${val}`)
-              .join("&");
+              .forEach(query => url.addQuery(query));
           } else {
             body = { ...body, ...data };
           }
@@ -37,14 +35,16 @@ export class ActionExecutor {
     }
 
     console.log(
-      `Doing a ${action.method} request to ${url} with body: ${JSON.stringify(body)}`,
+      `Doing a ${action.method} request to ${url} with body: ${JSON.stringify(
+        body
+      )}`
     );
 
     const options = {
       method: action.method,
-      url,
+      uri: url.toString(),
       body,
-      json: true,
+      json: true
     };
 
     return request(options);

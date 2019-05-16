40ed9947803edc127966f057ee8b2fe81c076727
Thomas Eizinger <thomas@eizinger.io>
2019-05-16 13:31:33 +1000

    Revise test to make it more readable

--- tests/wallets/ethereum.spec.ts
+++ tests/wallets/ethereum.spec.ts
@@ -1,56 +1,61 @@
 import BN from "bn.js";
+import utils from "ethereumjs-util";
 import Web3 from "web3";
 import { EthereumWallet } from "../../src/wallets/ethereum";
 import containerTest from "../containerTest";
 import EthereumHarness from "../ethereumHarness";
 import parityTestContainer from "../parityTestContainer";
 
+async function fundAddressOfPrivateKey(web3: Web3, privateKey: Buffer) {
+  const initialFunding = new BN(web3.utils.toWei("10", "ether"));
+  const address = "0x" + utils.privateToAddress(privateKey).toString("hex");
+
+  const ethereumHarness = new EthereumHarness(web3);
+  await ethereumHarness.fund(address, initialFunding);
+
+  // sanity check that we actually funded
+  expect(await web3.eth.getBalance(address)).toEqual(
+    initialFunding.toString(10)
+  );
+
+  return initialFunding;
+}
+
 describe("Ethereum Wallet", () => {
   test(
     "given initial funding, should transfer half of it with locally signed transaction",
     containerTest(parityTestContainer, async container => {
-      const endpoint = `http://localhost:${container.getMappedPort(8545)}`;
-      const web3 = new Web3(new Web3.providers.HttpProvider(endpoint));
-
-      const testAddress = "0xc2457d129c78bde6a4742b3ef38a7ba7f83d00b6";
       const testAddressPrivKey = Buffer.from(
         "1759af2005454dfc596d8e0d09469f5997069e8e6b24a82defa0dae4a387e5f7",
         "hex"
       );
-
-      const initialFunding = new BN(web3.utils.toWei("10", "ether"));
-
-      const wallet = new EthereumWallet(web3, testAddressPrivKey, 1);
-      const ethereumHarness = new EthereumHarness(web3);
-      await ethereumHarness.fund(testAddress, initialFunding);
-
-      expect(await web3.eth.getBalance(testAddress)).toEqual(
-        initialFunding.toString(10)
-      );
-
       const targetAddress = "0xcb777414c38e426c7038a7c4908751f5e864f7ad";
 
-      const balanceBefore = new BN(
-        await web3.eth.getBalance(targetAddress),
-        10
+      const web3 = new Web3(
+        new Web3.providers.HttpProvider(
+          `http://localhost:${container.getMappedPort(8545)}`
+        )
       );
+      const wallet = new EthereumWallet(web3, testAddressPrivKey, 1);
 
-      const amountToTransfer = new BN(web3.utils.toWei("10", "wei"));
+      const amountFounded = await fundAddressOfPrivateKey(
+        web3,
+        testAddressPrivKey
+      );
+      const amountToTransfer = amountFounded.divn(2);
       const gasPrice = new BN(0);
+
+      const balanceBefore = await web3.eth.getBalance(targetAddress);
       const receipt = await wallet.sendTransactionTo({
         to: targetAddress,
         value: amountToTransfer,
         gasPrice,
         gasLimit: new BN(100_000)
       });
+      const balanceAfter = await web3.eth.getBalance(targetAddress);
 
       const gasCost = new BN(receipt.gasUsed).mul(gasPrice);
-
-      const balanceAfter = new BN(
-        await web3.eth.getBalance(targetAddress),
-        10
-      ).toString(10);
-      const expectedBalance = balanceBefore
+      const expectedBalance = new BN(balanceBefore, 10)
         .add(amountToTransfer)
         .sub(gasCost)
         .toString(10);

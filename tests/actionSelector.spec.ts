d60953946f4448c913ff25d416ff359046b4e891
Franck Royer <franck@coblox.tech>
2019-06-08 12:47:02 +0800

    Add tests to ensure the rate logic is correct

--- tests/actionSelector.spec.ts
+++ tests/actionSelector.spec.ts
@@ -6,35 +6,35 @@ import swapsAcceptDeclineStub from "./stubs/bitcoinEther/swapsWithAcceptDecline.
 import swapsRedeemBitcoinEther from "./stubs/bitcoinEther/swapsWithRedeem.siren.json";
 import swapsFundEtherBitcoinStub from "./stubs/etherBitcoin/swapsWithFund.siren.json";
 
-describe("Action selector tests: ", () => {
-  const config = new Config({
-    comitNodeUrl: "http://localhost:8000",
-    seedWords:
-      "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon",
-    rates: { ether: { bitcoin: { sell: 0.0105, buy: 0.0105 } } },
-    ledgers: {
-      bitcoin: {
-        type: "coreRpc",
-        rpcUsername: "bitcoin",
-        rpcPassword: "password",
-        rpcHost: "127.0.0.1",
-        rpcPort: 18443,
-        network: "regtest",
-        fee: {
-          defaultFee: 10,
-          strategy: "hourFee"
-        }
-      },
-      ethereum: {
-        web3Endpoint: "http://localhost:8545",
-        fee: {
-          defaultFee: new BN(10),
-          strategy: "average"
-        }
+const config = new Config({
+  comitNodeUrl: "http://localhost:8000",
+  seedWords:
+    "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon",
+  rates: { ether: { bitcoin: { sell: 0.0105, buy: 0.0105 } } },
+  ledgers: {
+    bitcoin: {
+      type: "coreRpc",
+      rpcUsername: "bitcoin",
+      rpcPassword: "password",
+      rpcHost: "127.0.0.1",
+      rpcPort: 18443,
+      network: "regtest",
+      fee: {
+        defaultFee: 10,
+        strategy: "hourFee"
+      }
+    },
+    ethereum: {
+      web3Endpoint: "http://localhost:8545",
+      fee: {
+        defaultFee: new BN(10),
+        strategy: "average"
       }
     }
-  });
+  }
+});
 
+describe("Action selector tests: ", () => {
   function setupAction(stub: any) {
     const entity = stub.entities[0] as Entity;
     // @ts-ignore
@@ -103,6 +103,156 @@ describe("Action selector tests: ", () => {
 
     const actionResponse2 = await actionSelector.selectActions(entity);
     expect(actionResponse2).toBeUndefined();
+    done();
+  });
+});
+
+describe("Action selector rate tests", () => {
+  it("Should accept because Bob sells Ether and request is above the sell rate", async done => {
+    config.rates = { ether: { bitcoin: { sell: 0.001, buy: 0.01 } } };
+    const actionSelector = new ActionSelector(config);
+
+    const entity = swapsAcceptDeclineStub.entities[0] as Entity;
+    expect(entity.actions).not.toBeUndefined();
+
+    entity.properties = {
+      parameters: {
+        alpha_asset: {
+          name: "bitcoin",
+          quantity: "200000" // 0.002 Bitcoin
+        },
+        alpha_ledger: {
+          name: "bitcoin",
+          network: "regtest"
+        },
+        beta_asset: {
+          name: "ether",
+          quantity: "1000000000000000000" // 1 Ether
+        },
+        beta_ledger: {
+          name: "ethereum",
+          network: "regtest"
+        }
+      }
+    };
+
+    // @ts-ignore
+    const acceptStub = entity.actions[0] as Action;
+    expect(acceptStub.name).toBe("accept");
+
+    const actionResponse = await actionSelector.selectActions(entity);
+    expect(actionResponse).toStrictEqual(acceptStub);
+    done();
+  });
+
+  it("Should decline because Bob sells Ether and request is below the sell rate", async done => {
+    config.rates = { ether: { bitcoin: { sell: 0.001, buy: 0.01 } } };
+    const actionSelector = new ActionSelector(config);
+
+    const entity = swapsAcceptDeclineStub.entities[0] as Entity;
+    expect(entity.actions).not.toBeUndefined();
+
+    entity.properties = {
+      parameters: {
+        alpha_asset: {
+          name: "bitcoin",
+          quantity: "80000" // 0.0008 Bitcoin
+        },
+        alpha_ledger: {
+          name: "bitcoin",
+          network: "regtest"
+        },
+        beta_asset: {
+          name: "ether",
+          quantity: "1000000000000000000" // 1 Ether
+        },
+        beta_ledger: {
+          name: "ethereum",
+          network: "regtest"
+        }
+      }
+    };
+
+    // @ts-ignore
+    const declineStub = entity.actions[1] as Action;
+    expect(declineStub.name).toBe("decline");
+
+    const actionResponse = await actionSelector.selectActions(entity);
+    expect(actionResponse).toStrictEqual(declineStub);
+    done();
+  });
+
+  it("Should accept because Bob buys Ether and request is above the sell rate", async done => {
+    config.rates = { ether: { bitcoin: { sell: 0.001, buy: 0.01 } } };
+    const actionSelector = new ActionSelector(config);
+
+    const entity = swapsAcceptDeclineStub.entities[0] as Entity;
+    expect(entity.actions).not.toBeUndefined();
+
+    entity.properties = {
+      parameters: {
+        alpha_asset: {
+          name: "ether",
+          quantity: "1000000000000000000" // 1 Ether
+        },
+        alpha_ledger: {
+          name: "ethereum",
+          network: "regtest"
+        },
+        beta_asset: {
+          name: "bitcoin",
+          quantity: "900000" // 0.009 Bitcoin
+        },
+        beta_ledger: {
+          name: "bitcoin",
+          network: "regtest"
+        }
+      }
+    };
+
+    // @ts-ignore
+    const acceptStub = entity.actions[0] as Action;
+    expect(acceptStub.name).toBe("accept");
+
+    const actionResponse = await actionSelector.selectActions(entity);
+    expect(actionResponse).toStrictEqual(acceptStub);
+    done();
+  });
+
+  it("Should decline because Bob buys Ether and request is below the sell rate", async done => {
+    config.rates = { ether: { bitcoin: { sell: 0.001, buy: 0.01 } } };
+    const actionSelector = new ActionSelector(config);
+
+    const entity = swapsAcceptDeclineStub.entities[0] as Entity;
+    expect(entity.actions).not.toBeUndefined();
+
+    entity.properties = {
+      parameters: {
+        alpha_asset: {
+          name: "ether",
+          quantity: "1000000000000000000" // 1 Ether
+        },
+        alpha_ledger: {
+          name: "ethereum",
+          network: "regtest"
+        },
+        beta_asset: {
+          name: "bitcoin",
+          quantity: "2000000" // 0.02 Bitcoin
+        },
+        beta_ledger: {
+          name: "bitcoin",
+          network: "regtest"
+        }
+      }
+    };
+
+    // @ts-ignore
+    const declineStub = entity.actions[1] as Action;
+    expect(declineStub.name).toBe("decline");
+
+    const actionResponse = await actionSelector.selectActions(entity);
+    expect(actionResponse).toStrictEqual(declineStub);
     done();
   });
 });

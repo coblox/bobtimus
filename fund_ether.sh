499b0a8cf27fcdc30e9dc34ce388596a2e348ebc
Franck Royer <franck@coblox.tech>
2019-06-09 17:02:55 +0800

    Give them 10 Ether

--- fund_ether.sh
+++ fund_ether.sh
@@ -2,5 +2,5 @@
 
 TO=$1
 
-curl --data "{\"method\":\"personal_sendTransaction\",\"params\":[{\"from\":\"0x00a329c0648769a73afac7f9381e08fb43dbea72\",\"to\":\"${TO}\",\"value\":\"0xDE0B6B3A7640000\"},\"\"],\"id\":1,\"jsonrpc\":\"2.0\"}" -H "Content-Type: application/json" -X POST localhost:8545
+curl --data "{\"method\":\"personal_sendTransaction\",\"params\":[{\"from\":\"0x00a329c0648769a73afac7f9381e08fb43dbea72\",\"to\":\"${TO}\",\"value\":\"0x8AC7230489E80000\"},\"\"],\"id\":1,\"jsonrpc\":\"2.0\"}" -H "Content-Type: application/json" -X POST localhost:8545
 

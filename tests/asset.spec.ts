8630987b5115ccd7441560fc93bf61c20f0d9179
Franck Royer <franck@coblox.tech>
2019-08-05 17:14:21 +1000

    test: Convert integer ERC20 amount to float as per decimals

--- tests/asset.spec.ts
+++ tests/asset.spec.ts
@@ -13,7 +13,8 @@ describe("Test asset", () => {
       return new Asset(
         "PAY",
         Ledger.Ethereum,
-        "0xB97048628DB6B661D4C2aA833e95Dbe1A905B280"
+        "0xB97048628DB6B661D4C2aA833e95Dbe1A905B280",
+        18
       );
     };
 
@@ -32,10 +33,23 @@ describe("Test asset", () => {
       "0xB97048628DB6B661D4C2aA833e95Dbe1A905B280"
     );
     expect(asset.ledger).toEqual(Ledger.Ethereum);
+    expect(asset.decimals).toEqual(18);
   });
 
   it("Convert satoshis to Bitcoin", () => {
     const sats = new Big("100000000");
     expect(toNominalUnit(Asset.bitcoin, sats)).toEqual(new Big(1));
+  });
+
+  it("Convert ERC20 integer quantity to correct float as per configured decimals", () => {
+    const intQuantity = new Big("1000000000000000000");
+    const payAsset = new Asset(
+      "PAY",
+      Ledger.Ethereum,
+      "0xB97048628DB6B661D4C2aA833e95Dbe1A905B280",
+      18
+    );
+
+    expect(toNominalUnit(payAsset, intQuantity)).toEqual(new Big(1));
   });
 });

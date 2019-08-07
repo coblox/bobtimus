4e0be83399d69651da4b2bf5792c1e3de9667a66
Franck Royer <franck@coblox.tech>
2019-08-07 16:38:50 +1000

    test: Return ERC20 offer as part of published offers

--- tests/rates/testnetMarketMaker.spec.ts
+++ tests/rates/testnetMarketMaker.spec.ts
@@ -34,6 +34,13 @@ async function createMockBalances(
   return Balances.create(mockBalanceLookups, 20);
 }
 
+const payAsset = new Asset(
+  "PAY",
+  Ledger.Ethereum,
+  "0xB97048628DB6B661D4C2aA833e95Dbe1A905B280",
+  18
+);
+
 describe("Test the TestnetMarketMaker module", () => {
   const buyAsset = Asset.bitcoin;
   const sellAsset = Asset.ether;
@@ -265,12 +272,6 @@ describe("Test the TestnetMarketMaker module", () => {
   });
 
   it("Returns the amounts to publish for buy and sell assets with an ERC20 asset based on the balances, the configured published fraction and the rate spread", async () => {
-    const payAsset = new Asset(
-      "PAY",
-      Ledger.Ethereum,
-      "0xB97048628DB6B661D4C2aA833e95Dbe1A905B280",
-      18
-    );
     const offer: Offer = {
       timestamp: new Date(),
       protocol: "rfc003",
@@ -298,5 +299,66 @@ describe("Test the TestnetMarketMaker module", () => {
     expect(offersToPublish).toBeDefined();
     expect(offersToPublish.buy).toEqual(offer.buy); // Based on the publish fraction
     expect(offersToPublish.sell).toEqual(offer.sell);
+  });
+
+  it("Should return the trades, including ERC20, according to the balance", async () => {
+    const expected: List<Offer> = [
+      {
+        timestamp: new Date(),
+        protocol: "rfc003",
+        buy: {
+          ledger: Ledger.Bitcoin,
+          asset: Asset.bitcoin,
+          quantity: new Big(0.525)
+        },
+        sell: {
+          ledger: Ledger.Ethereum,
+          asset: Asset.ether,
+          quantity: new Big(5)
+        }
+      },
+      {
+        timestamp: new Date(),
+        protocol: "rfc003",
+        buy: {
+          ledger: Ledger.Ethereum,
+          asset: Asset.ether,
+          quantity: new Big(5.25)
+        },
+        sell: {
+          ledger: Ledger.Bitcoin,
+          asset: Asset.bitcoin,
+          quantity: new Big(0.5)
+        }
+      },
+      {
+        timestamp: new Date(),
+        protocol: "rfc003",
+        buy: {
+          ledger: Ledger.Ethereum,
+          asset: payAsset,
+          quantity: new Big(5.25)
+        },
+        sell: {
+          ledger: Ledger.Bitcoin,
+          asset: Asset.bitcoin,
+          quantity: new Big(0.5)
+        }
+      }
+    ];
+
+    const marketMaker = new TestnetMarketMaker(
+      { rateSpread: 5, publishFraction: 200, maxFraction: 100 },
+      await createMockBalances(100, 1000, 10000)
+    );
+
+    const trades = await marketMaker.prepareOffersToPublish();
+
+    expect(trades[0].buy).toEqual(expected[0].buy);
+    expect(trades[0].sell).toEqual(expected[0].sell);
+    expect(trades[1].buy).toEqual(expected[1].buy);
+    expect(trades[1].sell).toEqual(expected[1].sell);
+    expect(trades[2].buy).toEqual(expected[2].buy);
+    expect(trades[2].sell).toEqual(expected[2].sell);
   });
 });
